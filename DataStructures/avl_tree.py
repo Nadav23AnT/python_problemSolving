52a4caa33137acf1b9af1adf9c4b27bc94b88089
NadavChen <nadav2282@gmail.com>
2023-06-13 13:34:58 +0300

    Added AVL tree algorithem

---
+++ DataStructures/avl_tree.py
@@ -0,0 +1,101 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return Node(key)
+        elif key < node.key:
+            node.left = self._insert(node.left, key)
+        else:
+            node.right = self._insert(node.right, key)
+
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
+
+        balance = self._get_balance(node)
+
+        if balance > 1:
+            if key < node.left.key:
+                return self._rotate_right(node)
+            else:
+                node.left = self._rotate_left(node.left)
+                return self._rotate_right(node)
+
+        if balance < -1:
+            if key > node.right.key:
+                return self._rotate_left(node)
+            else:
+                node.right = self._rotate_right(node.right)
+                return self._rotate_left(node)
+
+        return node
+
+    def _rotate_left(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+    def _rotate_right(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+    def _get_height(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def _get_balance(self, node):
+        if node is None:
+            return 0
+        return self._get_height(node.left) - self._get_height(node.right)
+
+    def in_order_traversal(self):
+        self._in_order_traversal(self.root)
+
+    def _in_order_traversal(self, node, depth=0):
+        if node:
+            self._in_order_traversal(node.right, depth + 1)
+            print("    " * depth, end="")
+            if depth > 0:
+                print("|", end="")
+            print("__", node.key)
+            self._in_order_traversal(node.left, depth + 1)
+
+
+# Example
+avl_tree = AVLTree()
+avl_tree.insert(10)
+avl_tree.insert(20)
+avl_tree.insert(30)
+avl_tree.insert(40)
+avl_tree.insert(50)
+avl_tree.insert(25)
+
+avl_tree.in_order_traversal()
+
