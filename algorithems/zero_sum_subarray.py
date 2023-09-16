60b559004674bc1ff8151097896fb0e57d2e4507
NadavChen <nadav2282@gmail.com>
2023-09-16 17:45:56 +0300

    medium algorithems

---
+++ algorithems/zero_sum_subarray.py
@@ -0,0 +1,9 @@
+def zeroSumSubarray(nums):
+    sums = set([0])
+    currentSum = 0
+    for num in nums:
+        currentSum += num
+        if currentSum in sums:
+            return True
+        sums.add(currentSum)
+    return False
